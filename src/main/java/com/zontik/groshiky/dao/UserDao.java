e737cab73a3296da249d623ae5c851e2023777c1
017zontik <017zontik@gmail.com>
2020-06-11 21:39:42 +0300

    method findByLogin have been added

--- src/main/java/com/zontik/groshiky/dao/UserDao.java
+++ src/main/java/com/zontik/groshiky/dao/UserDao.java
@@ -1,15 +1,17 @@
 package com.zontik.groshiky.dao;
 
 import com.zontik.groshiky.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
+import javax.persistence.NoResultException;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
-public class UserDao implements IUserDao{
+public class UserDao implements IUserDao {
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -17,5 +19,17 @@ public class UserDao implements IUserDao{
     @Override
     public void createUser(User user) {
         sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            Query<User> findByUsername = session.createQuery("from User where login = :login");
+            findByUsername.setParameter("login", login);
+            return findByUsername.getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        }
     }
 }
