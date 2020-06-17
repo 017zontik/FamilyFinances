48bd51cf9447cf03e822ba0be3cf082d7306b746
017zontik <017zontik@gmail.com>
2020-06-17 13:58:12 +0300

    passwordEncoder and formLogin was added

--- src/main/java/com/zontik/groshiky/config/security/AppSecurityConfig.java
+++ src/main/java/com/zontik/groshiky/config/security/AppSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.zontik.groshiky.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +17,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +31,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/index").permitAll()
-                .antMatchers("/dashboard").authenticated();
+                    .antMatchers("/index").permitAll()
+                    .antMatchers("/dashboard").authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/index")
+                    .usernameParameter("login")
+                    .loginProcessingUrl("/perform_index")
+                    .defaultSuccessUrl("/dashboard");
     }
 }
