f04d8cf624ab8974de49a881840bc698655cddf4
017zontik <017zontik@gmail.com>
2020-06-23 22:42:58 +0300

    Method toString has been redefined

--- src/main/java/com/zontik/groshiky/model/Role.java
+++ src/main/java/com/zontik/groshiky/model/Role.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Table(name = "roles")
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+
 public class Role {
 
     @Id
@@ -23,4 +23,12 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private List<User> users;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
