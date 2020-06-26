f34c8e5b4b3be16d9298e4b57050aa035f8c78fd
017zontik <017zontik@gmail.com>
2020-06-26 20:55:42 +0300

    Enum OperationResult was added

---
+++ src/main/java/com/zontik/groshiky/model/OperationResult.java
@@ -0,0 +1,26 @@
+package com.zontik.groshiky.model;
+
+public enum  OperationResult {
+        NOTAUTHORISED,
+        REGISTERED
+
+
+//        NOTAUTHORISED("notAuthorised", "Wrong login or password"),
+//        REGISTERED("registered", "You have successfully registered. Please log in."),
+//        LOGINEXISTS("loginExists", "This login name is already taken. Please enter another one.");
+//
+//        private String name;
+//        private String description;
+//
+//        Result (String name, String description) {
+//            this.name = name;
+//            this.description = description;
+//        }
+//        public String getName() {
+//            return this.name;
+//        }
+//        public String getDescription() {
+//            return this.description;
+//        }
+
+}
