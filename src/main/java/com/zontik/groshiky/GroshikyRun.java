c14e12000d71325f77b31b631f5d15b6fa0558c1
017zontik <017zontik@gmail.com>
2020-06-30 22:10:52 +0300

    method main was added

---
+++ src/main/java/com/zontik/groshiky/GroshikyRun.java
@@ -0,0 +1,12 @@
+package com.zontik.groshiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroshikyRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroshikyRun.class, args);
+    }
+}
