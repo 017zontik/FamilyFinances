7ff137af48eab879a01c345633286c5a13742533
017zontik <017zontik@gmail.com>
2020-07-01 14:49:27 +0300

    method was renamed

--- src/main/java/com/zontik/groshiky/dao/IRoleDao.java
+++ src/main/java/com/zontik/groshiky/dao/IRoleDao.java
@@ -2,7 +2,11 @@ package com.zontik.groshiky.dao;
 
 import com.zontik.groshiky.model.Role;
 import com.zontik.groshiky.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IRoleDao {
-    Role findRoleUser(Roles role);
+
+@Repository
+public interface IRoleDao extends JpaRepository<Role, Integer> {
+    Role findRoleByName(Roles role);
 }
