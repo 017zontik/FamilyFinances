4274dc5eb565850fdba5ebd39388b054c0ead3e1
017zontik <017zontik@gmail.com>
2020-07-01 14:52:33 +0300

    extra imports have been removed

--- src/main/java/com/zontik/groshiky/controllers/IndexController.java
+++ src/main/java/com/zontik/groshiky/controllers/IndexController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -65,5 +62,4 @@ public class IndexController extends BaseController{
         model.addAttribute("accounts", user.getAccounts());
         return "dashboard";
     }
-
 }
