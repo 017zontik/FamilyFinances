de239ea304c5f564738aa5d127834609fc2f8882
017zontik <017zontik@gmail.com>
2020-07-05 18:53:35 +0300

    The modal window was added

--- src/main/webapp/scripts/dashboard.js
+++ src/main/webapp/scripts/dashboard.js
@@ -1,8 +1,33 @@
 /* globals Chart:false, feather:false */
 
 (function () {
-  'use strict'
+    'use strict'
 
-  feather.replace()
+    feather.replace();
+
+    $("#saveAccount").click(function () {
+        $.post("addAccount", {name: $("#name").val()}, function (response) {
+            if (response.result === "OK") {
+                $("#accountModal").modal("hide");
+                let $accountClone = $("#accountTemplate").clone();
+                $("a div div strong", $accountClone).text(response.account.name);
+                $(".text-success strong", $accountClone).text("0,00 BYN");
+                $(".text-danger", $accountClone).hide();
+                $("#accountsList").append($accountClone);
+                $accountClone.show();
+            } else {
+                $("#accountErrorMessage").text(response.message).show();
+            }
+        });
+    });
+    $("#accountModal").on("show.bs.modal", function () {
+        $("#accountErrorMessage").hide();
+        $("#name").val("");
+
+    })
+
+
 
 }())
+
+
