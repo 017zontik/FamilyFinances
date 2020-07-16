e01f8e7c4baf3045c80b9749a0ccef18662d2c93
017zontik <017zontik@gmail.com>
2020-07-16 15:24:45 +0300

    The form can be validated

--- src/main/webapp/scripts/dashboard.js
+++ src/main/webapp/scripts/dashboard.js
@@ -6,26 +6,28 @@
     feather.replace();
 
     $("#saveAccount").click(function () {
-        $.post("addAccount", {name: $("#name").val()}, function (response) {
-            if (response.result === "OK") {
-                $("#accountModal").modal("hide");
-                let $accountClone = $("#accountTemplate").clone();
-                $("a div div strong", $accountClone).text(response.account.name);
-                $(".text-success strong", $accountClone).text("0,00 BYN");
-                $(".text-danger", $accountClone).hide();
-                $("#accountsList").append($accountClone);
-                $accountClone.show();
-            } else {
-                $("#accountErrorMessage").text(response.message).show();
-            }
-        });
+        if ( $("#newAccount")[0].reportValidity()){
+            $.post("addAccount", {name: $("#name").val()}, function (response) {
+                if (response.result === "OK") {
+                    $("#accountModal").modal("hide");
+                    let $accountClone = $("#accountTemplate").clone();
+                    $("a div div strong", $accountClone).text(response.accountModel.name);
+                    $(".text-success strong", $accountClone).text("0,00 BYN");
+                    $(".text-danger", $accountClone).hide();
+                    $("#accountsList").append($accountClone);
+                    $accountClone.show();
+                } else {
+                    $("#accountErrorMessage").text(response.message).show();
+                }
+            });
+        }
+
     });
     $("#accountModal").on("show.bs.modal", function () {
         $("#accountErrorMessage").hide();
         $("#name").val("");
 
     })
-
 
 
 }())
