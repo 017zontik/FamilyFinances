295b5085128ac4a7fd5bbdbee41f56fe1933622f
017zontik <017zontik@gmail.com>
2020-07-20 01:05:13 +0300

    the function of viewing accounts has been added

--- src/main/webapp/scripts/dashboard.js
+++ src/main/webapp/scripts/dashboard.js
@@ -35,10 +35,34 @@
 
     });
 
-    $("li.nav-item[data]").click(function (clickAccountEvent) {
-        let $account = clickAccountEvent.target.closest("li");
+    $("#accountsList").click(function (clickAccountEvent) {
+        $(".highlight-account").removeClass("highlight-account");
+        let $account = $(clickAccountEvent.target.closest("li")).addClass("highlight-account");
         let accountName = $("div > strong", $account).text();
         $("main div h1").text(accountName);
+        let $transactionsClone = $("#transactionsTemplate").clone();
+        $("#transactions").remove();
+        $transactionsClone.attr("id", "transactions");
+        $("main").append($transactionsClone);
+        $.ajax("transactions", {
+            type: "GET",
+            data: {accountId: $account.attr("data")},
+            statusCode: {
+                200: function (response) {
+                    $.each(response, function (index, value) {
+                        let $row = $("<tr>");
+                        let $dateColumn = $("<td>").text(value.date);
+                        $row.append($dateColumn);
+                        let $nameColumn = $("<td>").text(value.name);
+                        $row.append($nameColumn);
+                        let $amountColumn = $("<td>").text((value.amount).toFixed(2));
+                        $row.append($amountColumn);
+                        $("table tbody", $transactionsClone).append($row);
+                    })
+                }
+            }
+        })
+        $transactionsClone.show();
     })
 
 
